75971768aeba36165a12e1577509f204271f94b3
mariahul1 <hul.maria88@gmail.com>
2024-05-31 23:18:31 +0200

    ajout code

--- src/extraire_phrases.py
+++ src/extraire_phrases.py
@@ -1,8 +1,15 @@
 # Code écrit par Maria Hul
 
+"""
+Le script est utilisé pour l'extraction des phrases de deux corpus - Europarl et Emea pour effectuer l'évaluation du moteur OpenNMT sur le corpus en
+formes flèchies autant que le corpus en lemmes. Le corpus en lemmes a besoin de traitements supplémentaires, indiqués dans un autre fichier disponible dans le dossier.
+
+La commande pour lancer le script: 
+python3 extraire_phrases.py 
+"""
 import os
 
-##Définition des fonctions de vérification d'existence des dossiers, d'extraction des phrases et de sauvegarde
+##Définition des fonctions de vérification d'existence des dossiers, d'extraction des phrases et de sauvegarde avant le traitement
 def verifier_si_le_dossier_existe(chemin_dossier):
     """
     La fonction vérifie si le dossier spécifié existe et s'il n'existe pas, la fonction le crée.
@@ -10,9 +17,10 @@ def verifier_si_le_dossier_existe(chemin_dossier):
     if not os.path.exists(chemin_dossier):
         os.makedirs(chemin_dossier)
 
+##La fonction d'extractions des deux de deux corpus
 def extraire_phrases(chemin_fichier, debut, fin):
     """
-    La fonction extrait des phrases d'un fichier de point 'debut' jusqu'au point 'fin'.
+    La fonction extrait des phrases des fichiers de point 'debut' jusqu'au point 'fin'. Les points de début et de la fin seront indiqués après.
     """
     with open(chemin_fichier, 'r', encoding='utf-8') as fichier:
         lignes = fichier.readlines()
@@ -20,18 +28,20 @@ def extraire_phrases(chemin_fichier, debut, fin):
 
 def sauvegarder_phrases(phrases, chemin_fichier):
     """
-    La fonction sauvegarde une liste de phrases dans un fichier.
+    La fonction sauvegarde une liste de phrases dans un fichier indiqué.
     """
     verifier_si_le_dossier_existe(os.path.dirname(chemin_fichier))
     with open(chemin_fichier, 'w', encoding='utf-8') as fichier:
         fichier.writelines(phrases)
 
-##Définitions des dossiers contenant les fichiers traités pour faciliter l'écriture de la fonction suivante
+##Définitions des dossiers contenant les fichiers traités pour faciliter le traitement de plusieurs fichiers
 repertoire_data = 'données_du_corpus'
 repertoire_europarl = 'EUROPARL-corpus'
 repertoire_emea = 'EMEA-corpus'
 
 # Extraction des lignes souhaitées et sauvegarde des données essentielles pour Run 1 et Run 2
+##Le script utilise les données tokenisés
+## Les nombres indiquent les points de début et de fin (les rangs des fichiers source), le dernier fichier est le fichier de sauvegarde des phrases extraites
 traitement_des_données = [
     # corpus de train
     (f'{repertoire_europarl}/Europarl.en-fr.en', 0, 100000, f'{repertoire_data}/Europarl_train_100k.en'),
